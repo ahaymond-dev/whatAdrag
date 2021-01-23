1e0572d99ea8da26c760a351e1981e2ec0ca75d8
Austen Haymond <ahaymon17@gmail.com>
2021-01-23 15:04:22 -0700

    added basics for draggable components

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Draggable from "react-draggable";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+   
+    render() {
+      return (
+        <Draggable
+          axis="x"
+          handle=".handle"
+          defaultPosition={{x: 0, y: 0}}
+          position={null}
+          grid={[25, 25]}
+          scale={1}
+          onStart={this.handleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}>
+          <div>
+            <div className="handle">Drag from here</div>
+            <div>This readme is really dragging on...</div>
+          </div>
+        </Draggable>
+      );
+    }
+  }
+   
+  ReactDOM.render(<App/>, document.body);
